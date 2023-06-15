29a7f55b42f55f7674f0d9cfde10e2dcc506a645
GOLDER303 <szymonstepien@interia.pl>
2023-06-15 16:30:00 +0200

    Add game manager script

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI scoreText;
+    [SerializeField] GameObject gameOverScreen;
+
+    private int playerScore;
+
+    public void AddScore(int score)
+    {
+        playerScore += score;
+        scoreText.text = playerScore.ToString();
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void GameOver()
+    {
+        gameOverScreen.SetActive(true);
+    }
+}
